d8b33488b6bcae4d09de07c632f1e10bfdb79d76
Toty-01 <caucheanthony@gmail.com>
2023-03-02 20:28:55 +0100

    environnements setup

---
+++ src/Controller/UserController.php
@@ -0,0 +1,28 @@
+<?php
+// src/Controller/UserController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+  #[Route('/carte', name: 'carte')]
+
+    public function notifications(): Response
+    {
+        // get the user information and notifications somehow
+        $userFirstName = 'Toto';
+        $userNotifications = ['Le nom de mon pied est jean', 'Les chaussettes sont chaudes'];
+
+        // the template path is the relative file path from `templates/`
+        return $this->render('user/notifications.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'user_first_name' => $userFirstName,
+            'notifications' => $userNotifications,
+        ]);
+    }
+}
