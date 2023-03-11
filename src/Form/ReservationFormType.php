b15df9121a198d1e7746935f2038edfb28e8aacc
Toty-01 <caucheanthony@gmail.com>
2023-03-11 20:24:38 +0100

    accueil + carte et menu en cours

--- src/Form/ReservationFormType.php
+++ src/Form/ReservationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Reservation;
+use Doctrine\DBAL\Types\TimeType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,18 +20,23 @@ class ReservationFormType extends AbstractType
             ])
             ->add('couverts', options: [
                 'attr' => [
-                'class' => 'form-control w-75 mx-auto'
+                'class' => 'form-control w-75 mx-auto',
                 ]
             ])
             ->add('date', options: [
+                'widget' => 'single_text',
+
                 'attr' => [
                 'class' => 'form-control w-75 mx-auto'
                 ]
             ])
-            ->add('heure', options: [
+            ->add('heure',  options: [
                 'attr' => [
                 'class' => 'form-control w-75 mx-auto'
-                ]
+                ],
+                'widget' => 'choice',
+                'hours' => range(12, 20, 1),
+                'minutes' => range(0, 45, 15)
             ])
             ->add('allergie', options: [
                 'attr' => [
